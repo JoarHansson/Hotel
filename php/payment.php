5b1cd5ea9cf2ab8843c77d41532c6396ef21e998
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2023-12-18 16:19:45 +0100

    add guzzle and use the API in payment.php

--- php/payment.php
+++ php/payment.php
@@ -6,6 +6,15 @@ session_start();
 
 $db = new PDO("sqlite:" . __DIR__ . "/../database/hotel.db");
 
+require __DIR__ . "/../vendor/autoload.php";
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+$client = new Client();
+$baseUri = "https://www.yrgopelag.se/centralbank/";
+
+
 if (isset($_POST["guest-name"], $_POST["transfer-code"])) {
   $guestName = htmlspecialchars($_POST["guest-name"]);
   $transferCode = htmlspecialchars($_POST["transfer-code"]);
@@ -13,7 +22,21 @@ if (isset($_POST["guest-name"], $_POST["transfer-code"])) {
   $dateFrom = $_SESSION["reservedDateFrom"];
   $dateTo = $_SESSION["reservedDateTo"];
 
-  if (1 == 1) {  // placeholder for: if transfer code is valid...
+  try {
+    $responseCheckTransferCode = $client->request("POST", $baseUri . "transferCode", [
+      "form_params" => [
+        "transferCode" => $transferCode,
+        "totalcost" => "3" /* hard coded for now */
+      ]
+    ]);
+  } catch (ClientException $e) {
+    echo $e->getMessage();
+  }
+
+  $transferCodeStatus = json_decode($responseCheckTransferCode->getBody()->getContents());
+
+  // If transfer code is valid...
+  if ($transferCodeStatus->transferCode === $transferCode) {
 
     // save the booking info (insert into bookings table):
     $statementSaveBookingInfo = $db->prepare(
@@ -27,6 +50,18 @@ if (isset($_POST["guest-name"], $_POST["transfer-code"])) {
     $statementSaveBookingInfo->execute();
 
     $_SESSION["message"] = "payment succeeded";
+
+    // deposit the tc to my account
+    try {
+      $responseDepositTransferCode = $client->request("POST", $baseUri . "deposit", [
+        "form_params" => [
+          "user" => "Joar",
+          "transferCode" => $transferCode
+        ]
+      ]);
+    } catch (ClientException $e) {
+      echo $e->getMessage();
+    }
   } else {
 
     // if transfer code isn't valid, release the reservation
