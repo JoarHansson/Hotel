fd8ff670afe78c206164ffa4679a06b590049d51
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-01-05 16:55:59 +0100

    add hotel star rating to the header

--- php/header.php
+++ php/header.php
@@ -16,10 +16,21 @@
   <div class="absolute top-0 bottom-0 left-0 right-0 bg-[url('/assets/ice.jpg')] bg-center bg-fixed bg-auto opacity-50 -z-10"></div>
 
   <header class="w-full bg-cyan-300 p-4 mb-16 ">
-    <form action="index.php" method="post" class="flex justify-between items-center">
-      <button type="submit" class="text-4xl font-display italic uppercase text-cyan-950" name="pageState" value="home">The Ice Hotel</button>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <form action="index.php" method="post" class="flex justify-between items-center gap-4">
+      <button type="submit" class="text-2xl sm:text-4xl font-display italic uppercase text-cyan-950" name="pageState" value="home">The Ice hotel</button>
+      <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
         <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z" fill="currentColor" class="stroke-cyan-950 fill-cyan-950"></path>
-      </svg>
+      </svg> -->
+      <div class="flex gap-1 sm:gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-6 sm:w-8 h-6 sm:h-8">
+          <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-6 sm:w-8 h-6 sm:h-8">
+          <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-6 sm:w-8 h-6 sm:h-8">
+          <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+        </svg>
+      </div>
     </form>
   </header>
