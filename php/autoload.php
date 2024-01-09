678fa49acd58d699675af84827f71a9bc21a3737
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-01-09 14:02:18 +0100

    update star rating of the hotel

--- php/autoload.php
+++ php/autoload.php
@@ -7,13 +7,14 @@ require_once __DIR__ . "/functions.php";
 session_start();
 
 if (empty($_SESSION["reservation"])) {
-  $_SESSION["reservation"]["island"] = "The Island";
+  $_SESSION["reservation"]["island"] = "Mount Frost";
   $_SESSION["reservation"]["hotel"] = "The Ice Hotel";
-  $_SESSION["reservation"]["stars"] = "3";
+  $_SESSION["reservation"]["stars"] = "4";
   // currently:
   // - graphical presentation of the availability
   // - features
-  // - discounts?
+  // - discounts
+  // - admin page
 }
 
 $db = new PDO("sqlite:" . __DIR__ . "/../database/hotel.db");
--- php/header.php
+++ php/header.php
@@ -18,10 +18,10 @@
   <header class="w-full bg-cyan-300 p-4 mb-16 ">
     <form action="index.php" method="post" class="flex justify-between items-center gap-4">
       <button type="submit" class="text-2xl sm:text-4xl font-display italic uppercase text-cyan-950" name="pageState" value="home">The Ice hotel</button>
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-        <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z" fill="currentColor" class="stroke-cyan-950 fill-cyan-950"></path>
-      </svg> -->
       <div class="flex gap-1 sm:gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-6 sm:w-8 h-6 sm:h-8">
+          <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
+        </svg>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-6 sm:w-8 h-6 sm:h-8">
           <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
         </svg>
